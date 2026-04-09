18dddf5e7e584d5021040925dddcbfccd1b6ad59
sonaliparida9 <sonaliparida805@gmail.com>
2026-04-09 23:23:58 +0530

    practice hooks

---
+++ Counter App/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={()=>setCount(count + 1)}>+</button>
+      <button onClick={()=>setCount(count - 1)}>-</button>
+      <button onClick={()=>setCount(0)}>Reset</button>
+    </div>
+  )
+}
+
+export default App
